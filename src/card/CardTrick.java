b5581bf67557c269cea8533aafbe73060b226eb4
Haryanvi1 <133692620+Haryanvi1@users.noreply.github.com>
2023-05-31 15:31:38 -0400

    Update CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -11,52 +11,34 @@ import java.util.Scanner;
  * @author Ankit Ankit
  */
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
+
+        for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            c.setValue((int) (Math.random() * 13 + 1));  
-            c.setSuit(Card.SUITS[(int) (Math.random() * 4)]);  
+            c.setValue((int) (Math.random() * 13 + 1));
+            c.setSuit(Card.SUITS[(int) (Math.random() * 4)]);
             magicHand[i] = c;
         }
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the value of your card (1-13): ");
-        int userValue = input.nextInt();
-        System.out.print("Enter the suit of your card (0-3): ");
-        int userSuit = input.nextInt();
 
-        Card userCard = new Card();
-        userCard.setValue(userValue);
-        userCard.setSuit(Card.SUITS[userSuit]);
+        // Create the lucky card object with hard-coded values
+        Card luckyCard = new Card();
+        luckyCard.setValue(10); // Example value
+        luckyCard.setSuit("Hearts"); // Example suit
 
         boolean found = false;
         for (int i = 0; i < magicHand.length; i++) {
-            if (magicHand[i].getValue() == userCard.getValue() && magicHand[i].getSuit().equals(userCard.getSuit())) {
+            if (magicHand[i].equals(luckyCard)) {
                 found = true;
                 break;
             }
         }
 
         if (found) {
-            System.out.println("Your card is in the magic hand!");
+            System.out.println("Congratulations! Your lucky card is in the magic hand!");
         } else {
-            System.out.println("Your card is not in the magic hand.");
-        }
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+            System.out.println("Oops! Your lucky card is not in the magic hand.");
+        }
+    }
 }
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-        }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-// name is ankit
-   
-    
